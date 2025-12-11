cbd88cf8772562c677e6ac0a47d98cbc0a4edb6f
Seethal Paluri <s_paluri@apple.com>
2025-12-11 23:17:14 +0000

    Align the coding descriptor for mtpi_frame_presentation_length
    Fixes #1192

--- av2/decoder/obu.c
+++ av2/decoder/obu.c
@@ -976,7 +976,7 @@ static void read_metadata_temporal_point_info(AV2Decoder *const pbi,
                                               struct avm_read_bit_buffer *rb) {
   AV2_COMMON *const cm = &pbi->common;
   cm->temporal_point_info_metadata.mtpi_frame_presentation_length =
-      avm_rb_read_literal(rb, 5) + 1;
+      avm_rb_read_unsigned_literal(rb, 5) + 1;
   int n = cm->temporal_point_info_metadata.mtpi_frame_presentation_length;
   cm->temporal_point_info_metadata.mtpi_frame_presentation_time =
       avm_rb_read_literal(rb, n);

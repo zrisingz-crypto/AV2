df2913b7e6dd0feac16952224bedfb036b8dfa75
Tianqi Liu <tianqitliu@global.tencent.com>
2025-12-19 00:45:36 +0000

    Revert the aspect 4 in F303 for MHCCP division
    Revert the aspect 4 in F303 for MHCCP division.
    
    STATS_CHANGED

--- av2/common/cfl.c
+++ av2/common/cfl.c
@@ -1031,16 +1031,14 @@ void get_division_scale_shift(uint32_t denom, int32_t *scale, int32_t *round,
   // (squared) term in the polynomial for each of the 8 regions.
   static const int pow2W[DIV_PREC_BITS_POW2] = { 214, 153, 113, 86,
                                                  67,  53,  43,  35 };
-  static const int pow2Q[DIV_PREC_BITS_POW2] = { 227, 181, 148, 124,
-                                                 104, 89,  77,  68 };
   // This array contains the offset values used to adjust
   //  the normalized denominator for each region.
-  static const int pow2O[DIV_PREC_BITS_POW2] = { 1024, 3072,  5120,  7168,
-                                                 9216, 11264, 13312, 15360 };
+  static const int pow2O[DIV_PREC_BITS_POW2] = { 4822, 5952, 6624, 6792,
+                                                 6408, 5424, 3792, 1466 };
 
   // This array holds the constant bias term for each region's polynomial.
-  static const int pow2B[DIV_PREC_BITS_POW2] = { 15420, 13797, 12483, 11397,
-                                                 10485, 9709,  9039,  8456 };
+  static const int pow2B[DIV_PREC_BITS_POW2] = { 12784, 12054, 11670, 11583,
+                                                 11764, 12195, 12870, 13782 };
 
   *shift = floorLog2Uint64(denom);
   assert(*shift < 32);
@@ -1089,9 +1087,10 @@ void get_division_scale_shift(uint32_t denom, int32_t *scale, int32_t *round,
   int32_t index = normDiff >> DIV_INTR_BITS;
   int32_t normDiff2 = normDiff - pow2O[index];
 
-  *scale = (((pow2W[index] * ((normDiff2 * normDiff2) >> DIV_PREC_BITS)) >>
-             DIV_PREC_BITS_POW2) -
-            ((pow2Q[index] * normDiff2) >> DIV_PREC_BITS_POW2) + pow2B[index]);
+  *scale = ((pow2W[index] * ((normDiff2 * normDiff2) >> DIV_PREC_BITS)) >>
+            DIV_PREC_BITS_POW2) -
+           (normDiff2 >> 1) + pow2B[index];
+
   *scale <<= MHCCP_DECIM_BITS - DIV_PREC_BITS;
 }
 

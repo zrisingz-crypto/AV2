zrisingz-crypto/AV2

commit 8b5edc1520340764c60d3f09fb366061f3816b7b

CWG-F447: Extended dependency map signaling for tlayer
- Allows signaling different tlayer_dependency_map per mlayer
- Introduced a new flag to enable this feature (multi_tlayer_dependency_map_present_flag)
- This implementation is referred as option 1 in CWG-F447. 
- This commit does not include the metadata part in CWG-F447.

This commit does not impact the BD-rates under the CTC.

av2/decoder/decodeframe.c
@@ -3506,9 +3506,10 @@ void setup_quant_matrices(AV2Decoder *pbi, CommonQuantParams *quant_params,
   //  the sequence header or an embedded and temporal layer with ids qmMlayerId
   //  and qmTlayerId, respectively for which
   //  mlayer_dependency_map[obu_mlayer_id][qmMlayerId] and
-  //  tlayer_dependency_map[obu_tlayer_id][qmTlayerId] are both equal to 1.
+  //  tlayer_dependency_map[obu_mlayer_id][obu_tlayer_id][qmTlayerId] are both
+  //  equal to 1.
   if ((qmset->qm_tlayer_id != -1 &&
-       cm->seq_params.tlayer_dependency_map[cm->tlayer_id]
+       cm->seq_params.tlayer_dependency_map[cm->mlayer_id][cm->tlayer_id]
                                            [qmset->qm_tlayer_id] != 1) ||
       (qmset->qm_mlayer_id != -1 &&
        cm->seq_params.mlayer_dependency_map[cm->mlayer_id]
@@ -5835,8 +5836,9 @@ static void setup_film_grain(AV2Decoder *pbi, struct avm_read_bit_buffer *rb) {
       }
       if (!seq_params->mlayer_dependency_map
                [cm->mlayer_id][pbi->fgm_list[cm->fgm_id].fgm_mlayer_id] ||
-          !seq_params->tlayer_dependency_map
-               [cm->tlayer_id][pbi->fgm_list[cm->fgm_id].fgm_tlayer_id]) {
+          !seq_params->tlayer_dependency_map[cm->mlayer_id][cm->tlayer_id]
+                                            [pbi->fgm_list[cm->fgm_id]
+                                                 .fgm_tlayer_id]) {
         avm_internal_error(&cm->error, AVM_CODEC_UNSUP_BITSTREAM,
                            "mlayer_id(%d) or tlayer_id(%d) of the film grain "
                            "model is out of the limit",
@@ -6948,7 +6950,8 @@ static INLINE int get_disp_order_hint(AV2_COMMON *const cm)
         buf->is_restricted ||
 #endif  // CONFIG_F322_OBUER_REFRESTRICT
         !is_tlayer_scalable_and_dependent(&cm->seq_params, cm->tlayer_id,
-                                          buf->temporal_layer_id) ||
+                                          buf->temporal_layer_id,
+                                          cm->mlayer_id) ||
         !is_mlayer_scalable_and_dependent(&cm->seq_params, cm->mlayer_id,
                                           buf->mlayer_id))
       continue;
@@ -7276,7 +7279,8 @@ static int read_show_existing_frame(AV2Decoder *pbi,
       const RefCntBuffer *const buf = cm->ref_frame_map[map_idx];
       if (buf == NULL ||
           !is_tlayer_scalable_and_dependent(&cm->seq_params, cm->tlayer_id,
-                                            buf->temporal_layer_id) ||
+                                            buf->temporal_layer_id,
+                                            cm->mlayer_id) ||
           !is_mlayer_scalable_and_dependent(&cm->seq_params, cm->mlayer_id,
                                             buf->mlayer_id))
         continue;
@@ -8348,7 +8352,7 @@ static int read_uncompressed_header(AV2Decoder *pbi, OBU_TYPE obu_type,
           const int cur_tlayer_id = current_frame->temporal_layer_id;
           const int ref_tlayer_id = cm->ref_frame_map[ref]->temporal_layer_id;
           if (!is_tlayer_scalable_and_dependent(seq_params, cur_tlayer_id,
-                                                ref_tlayer_id)) {
+                                                ref_tlayer_id, cur_mlayer_id)) {
             avm_internal_error(
                 &cm->error, AVM_CODEC_UNSUP_BITSTREAM,
                 "Unsupported bitstream: temporal layer scalability shall be "

av2/common/av2_common_int.h
@@ -1117,8 +1117,10 @@ typedef struct SequenceHeader {
   int tlayer_dependency_present_flag;
   int mlayer_dependency_present_flag;
   // Layer dependency structure arrays
-  int tlayer_dependency_map[MAX_NUM_TLAYERS][MAX_NUM_TLAYERS];
+  int tlayer_dependency_map[MAX_NUM_MLAYERS][MAX_NUM_TLAYERS][MAX_NUM_TLAYERS];
   int mlayer_dependency_map[MAX_NUM_MLAYERS][MAX_NUM_MLAYERS];
+  // Flag indicating tlayer map signaling per mlayer
+  int multi_tlayer_dependency_map_present_flag;
 
   uint8_t df_par_bits_minus2;
 
@@ -3094,20 +3096,27 @@ static INLINE int get_ref_frame_map_idx(const AV2_COMMON *const cm,
 
 static INLINE void setup_default_temporal_layer_dependency_structure(
     SequenceHeader *const seq) {
-  const int max_layer_id = seq->max_tlayer_id;
+  const int max_mlayer_id = seq->max_mlayer_id;
+  const int max_tlayer_id = seq->max_tlayer_id;
   memset(seq->tlayer_dependency_map, 0, sizeof seq->tlayer_dependency_map);
-  for (int curr_layer_id = 0; curr_layer_id <= max_layer_id; curr_layer_id++) {
-    for (int ref_layer_id = 0; ref_layer_id <= curr_layer_id; ref_layer_id++) {
-      seq->tlayer_dependency_map[curr_layer_id][ref_layer_id] = 1;
+  for (int mlayer_id = 0; mlayer_id <= max_mlayer_id; mlayer_id++) {
+    for (int curr_tlayer_id = 0; curr_tlayer_id <= max_tlayer_id;
+         curr_tlayer_id++) {
+      for (int ref_tlayer_id = 0; ref_tlayer_id <= curr_tlayer_id;
+           ref_tlayer_id++) {
+        seq->tlayer_dependency_map[mlayer_id][curr_tlayer_id][ref_tlayer_id] =
+            1;
+      }
     }
   }
 }
 
 static INLINE int is_tlayer_scalable_and_dependent(
-    const SequenceHeader *const seq, const int curr_layer_id,
-    const int ref_layer_id) {
-  assert(seq->max_tlayer_id >= curr_layer_id &&
-         seq->max_tlayer_id >= ref_layer_id);
+    const SequenceHeader *const seq, const int curr_tlayer_id,
+    const int ref_tlayer_id, const int curr_mlayer_id) {
+  assert(seq->max_tlayer_id >= curr_tlayer_id &&
+         seq->max_tlayer_id >= ref_tlayer_id);
+  assert(seq->max_tlayer_id >= curr_mlayer_id);
   // clang-format off
   /* The additional conditional check based on 'tlayer_dependency_present_flag' is
   redundant, since tlayer_dependency_map[][] equivalently implements
@@ -3124,9 +3133,10 @@ static INLINE int is_tlayer_scalable_and_dependent(
   */
   // clang-format on
   if (seq->tlayer_dependency_present_flag) {
-    return seq->tlayer_dependency_map[curr_layer_id][ref_layer_id];
+    return seq
+        ->tlayer_dependency_map[curr_mlayer_id][curr_tlayer_id][ref_tlayer_id];
   } else {
-    return curr_layer_id >= ref_layer_id;
+    return curr_tlayer_id >= ref_tlayer_id;
   }
 }
 

av2/common/pred_common.c
@@ -188,7 +188,7 @@ int av2_get_op_constrained_ref_frames(AV2_COMMON *cm, int cur_frame_disp,
     const int cur_temporal_id = cm->current_frame.temporal_layer_id;
     const int ref_temporal_id = cur_ref.temporal_layer_id;
     if (!is_tlayer_scalable_and_dependent(&cm->seq_params, cur_temporal_id,
-                                          ref_temporal_id) ||
+                                          ref_temporal_id, cur_mlayer_id) ||
         !is_mlayer_scalable_and_dependent(&cm->seq_params, cur_mlayer_id,
                                           ref_mlayer_id))
       continue;
@@ -317,7 +317,7 @@ int av2_get_ref_frames(AV2_COMMON *cm, int cur_frame_disp,
     const int cur_temporal_id = cm->current_frame.temporal_layer_id;
     const int ref_temporal_id = cur_ref.temporal_layer_id;
     if (!is_tlayer_scalable_and_dependent(&cm->seq_params, cur_temporal_id,
-                                          ref_temporal_id) ||
+                                          ref_temporal_id, cur_mlayer_id) ||
         !is_mlayer_scalable_and_dependent(&cm->seq_params, cur_mlayer_id,
                                           ref_mlayer_id))
       continue;

av2/common/pred_common.h
@@ -56,9 +56,9 @@ static INLINE void init_ref_map_pair(AV2_COMMON *cm,
     if (ref_frame_map_pairs[map_idx].ref_frame_for_inference == -1) continue;
     ref_frame_map_pairs[map_idx].ref_frame_for_inference = 1;
     if (buf == NULL ||
-        !is_tlayer_scalable_and_dependent(&cm->seq_params,
-                                          cm->current_frame.temporal_layer_id,
-                                          buf->temporal_layer_id) ||
+        !is_tlayer_scalable_and_dependent(
+            &cm->seq_params, cm->current_frame.temporal_layer_id,
+            buf->temporal_layer_id, cm->current_frame.mlayer_id) ||
         !is_mlayer_scalable_and_dependent(
             &cm->seq_params, cm->current_frame.mlayer_id, buf->mlayer_id) ||
         (is_ras && buf->frame_type != KEY_FRAME)) {

av2/decoder/obu.c
@@ -85,21 +85,36 @@ static int read_bitstream_level(AV2_LEVEL *seq_level_idx,
 
 static void av2_read_tlayer_dependency_info(SequenceHeader *const seq,
                                             struct avm_read_bit_buffer *rb) {
-  const int max_layer_id = seq->max_tlayer_id;
-  for (int curr_layer_id = 1; curr_layer_id <= max_layer_id; curr_layer_id++) {
-    for (int ref_layer_id = curr_layer_id; ref_layer_id >= 0; ref_layer_id--) {
-      seq->tlayer_dependency_map[curr_layer_id][ref_layer_id] =
-          avm_rb_read_bit(rb);
+  const int max_mlayer_id = seq->max_mlayer_id;
+  const int max_tlayer_id = seq->max_tlayer_id;
+  const int multi_tlayer_flag = seq->multi_tlayer_dependency_map_present_flag;
+  for (int curr_mlayer_id = 0; curr_mlayer_id <= max_mlayer_id;
+       curr_mlayer_id++) {
+    for (int curr_tlayer_id = 1; curr_tlayer_id <= max_tlayer_id;
+         curr_tlayer_id++) {
+      for (int ref_tlayer_id = curr_tlayer_id; ref_tlayer_id >= 0;
+           ref_tlayer_id--) {
+        if (multi_tlayer_flag > 0 || curr_mlayer_id == 0) {
+          seq->tlayer_dependency_map[curr_mlayer_id][curr_tlayer_id]
+                                    [ref_tlayer_id] = avm_rb_read_bit(rb);
+        } else {
+          seq->tlayer_dependency_map[curr_mlayer_id][curr_tlayer_id]
+                                    [ref_tlayer_id] =
+              seq->tlayer_dependency_map[0][curr_tlayer_id][ref_tlayer_id];
+        }
+      }
     }
   }
 }
 
 static void av2_read_mlayer_dependency_info(SequenceHeader *const seq,
                                             struct avm_read_bit_buffer *rb) {
-  const int max_layer_id = seq->max_mlayer_id;
-  for (int curr_layer_id = 1; curr_layer_id <= max_layer_id; curr_layer_id++) {
-    for (int ref_layer_id = curr_layer_id; ref_layer_id >= 0; ref_layer_id--) {
-      seq->mlayer_dependency_map[curr_layer_id][ref_layer_id] =
+  const int max_mlayer_id = seq->max_mlayer_id;
+  for (int curr_mlayer_id = 1; curr_mlayer_id <= max_mlayer_id;
+       curr_mlayer_id++) {
+    for (int ref_mlayer_id = curr_mlayer_id; ref_mlayer_id >= 0;
+         ref_mlayer_id--) {
+      seq->mlayer_dependency_map[curr_mlayer_id][ref_mlayer_id] =
           avm_rb_read_bit(rb);
     }
   }
@@ -490,19 +505,10 @@ static uint32_t read_sequence_header_obu(AV2Decoder *pbi,
     seq_params->max_mlayer_id = avm_rb_read_literal(rb, MLAYER_BITS);
   }
 
-  // setup default temporal layer dependency
-  setup_default_temporal_layer_dependency_structure(seq_params);
   // setup default embedded layer dependency
   setup_default_embedded_layer_dependency_structure(seq_params);
-
-  // tlayer dependency description
-  seq_params->tlayer_dependency_present_flag = 0;
-  if (seq_params->max_tlayer_id > 0) {
-    seq_params->tlayer_dependency_present_flag = avm_rb_read_bit(rb);
-    if (seq_params->tlayer_dependency_present_flag) {
-      av2_read_tlayer_dependency_info(seq_params, rb);
-    }
-  }
+  // setup default temporal layer dependency
+  setup_default_temporal_layer_dependency_structure(seq_params);
 
   // mlayer dependency description
   seq_params->mlayer_dependency_present_flag = 0;
@@ -510,6 +516,20 @@ static uint32_t read_sequence_header_obu(AV2Decoder *pbi,
     seq_params->mlayer_dependency_present_flag = avm_rb_read_bit(rb);
     if (seq_params->mlayer_dependency_present_flag) {
       av2_read_mlayer_dependency_info(seq_params, rb);
+    }
+  }
+
+  // tlayer dependency description
+  seq_params->tlayer_dependency_present_flag = 0;
+  seq_params->multi_tlayer_dependency_map_present_flag = 0;
+  if (seq_params->max_tlayer_id > 0) {
+    seq_params->tlayer_dependency_present_flag = avm_rb_read_bit(rb);
+    if (seq_params->tlayer_dependency_present_flag) {
+      if (seq_params->max_mlayer_id > 0) {
+        seq_params->multi_tlayer_dependency_map_present_flag =
+            avm_rb_read_bit(rb);
+      }
+      av2_read_tlayer_dependency_info(seq_params, rb);
     }
   }
 

av2/encoder/bitstream.c
@@ -6207,22 +6207,38 @@ static AVM_INLINE void write_bitstream_level(AV2_LEVEL seq_level_idx,
 
 static void av2_write_tlayer_dependency_info(struct avm_write_bit_buffer *wb,
                                              const SequenceHeader *const seq) {
-  const int max_layer_id = seq->max_tlayer_id;
-  for (int curr_layer_id = 1; curr_layer_id <= max_layer_id; curr_layer_id++) {
-    for (int ref_layer_id = curr_layer_id; ref_layer_id >= 0; ref_layer_id--) {
-      avm_wb_write_bit(wb,
-                       seq->tlayer_dependency_map[curr_layer_id][ref_layer_id]);
+  const int max_mlayer_id = seq->max_mlayer_id;
+  const int max_tlayer_id = seq->max_tlayer_id;
+  const int multi_tlayer_flag = seq->multi_tlayer_dependency_map_present_flag;
+  for (int curr_mlayer_id = 0; curr_mlayer_id <= max_mlayer_id;
+       curr_mlayer_id++) {
+    for (int curr_tlayer_id = 1; curr_tlayer_id <= max_tlayer_id;
+         curr_tlayer_id++) {
+      for (int ref_tlayer_id = curr_tlayer_id; ref_tlayer_id >= 0;
+           ref_tlayer_id--) {
+        if (multi_tlayer_flag > 0 || curr_mlayer_id == 0) {
+          avm_wb_write_bit(
+              wb, seq->tlayer_dependency_map[curr_mlayer_id][curr_tlayer_id]
+                                            [ref_tlayer_id]);
+        } else {
+          assert(seq->tlayer_dependency_map[0][curr_tlayer_id][ref_tlayer_id] ==
+                 seq->tlayer_dependency_map[curr_mlayer_id][curr_tlayer_id]
+                                           [ref_tlayer_id]);
+        }
+      }
     }
   }
 }
 
 static void av2_write_mlayer_dependency_info(struct avm_write_bit_buffer *wb,
                                              const SequenceHeader *const seq) {
-  const int max_layer_id = seq->max_mlayer_id;
-  for (int curr_layer_id = 1; curr_layer_id <= max_layer_id; curr_layer_id++) {
-    for (int ref_layer_id = curr_layer_id; ref_layer_id >= 0; ref_layer_id--) {
-      avm_wb_write_bit(wb,
-                       seq->mlayer_dependency_map[curr_layer_id][ref_layer_id]);
+  const int max_mlayer_id = seq->max_mlayer_id;
+  for (int curr_mlayer_id = 1; curr_mlayer_id <= max_mlayer_id;
+       curr_mlayer_id++) {
+    for (int ref_mlayer_id = curr_mlayer_id; ref_mlayer_id >= 0;
+         ref_mlayer_id--) {
+      avm_wb_write_bit(
+          wb, seq->mlayer_dependency_map[curr_mlayer_id][ref_mlayer_id]);
     }
   }
 }
@@ -6345,19 +6361,23 @@ uint32_t av2_write_sequence_header_obu(const SequenceHeader *seq_params,
     avm_wb_write_literal(&wb, seq_params->max_mlayer_id, MLAYER_BITS);
   }
 
-  // tlayer dependency description
-  if (seq_params->max_tlayer_id > 0) {
-    avm_wb_write_bit(&wb, seq_params->tlayer_dependency_present_flag);
-    if (seq_params->tlayer_dependency_present_flag) {
-      av2_write_tlayer_dependency_info(&wb, seq_params);
-    }
-  }
-
   // mlayer dependency description
   if (seq_params->max_mlayer_id > 0) {
     avm_wb_write_bit(&wb, seq_params->mlayer_dependency_present_flag);
     if (seq_params->mlayer_dependency_present_flag) {
       av2_write_mlayer_dependency_info(&wb, seq_params);
+    }
+  }
+
+  // tlayer dependency description
+  if (seq_params->max_tlayer_id > 0) {
+    avm_wb_write_bit(&wb, seq_params->tlayer_dependency_present_flag);
+    if (seq_params->tlayer_dependency_present_flag) {
+      if (seq_params->max_mlayer_id > 0) {
+        avm_wb_write_bit(&wb,
+                         seq_params->multi_tlayer_dependency_map_present_flag);
+      }
+      av2_write_tlayer_dependency_info(&wb, seq_params);
     }
   }
 

av2/encoder/encoder.c
@@ -4865,7 +4865,8 @@ int av2_encode(AV2_COMP *const cpi, uint8_t *const dest,
       if (cm->ref_frame_map[ref_idx] != NULL) {
         if (!is_tlayer_scalable_and_dependent(
                 &cm->seq_params, current_frame->temporal_layer_id,
-                cm->ref_frame_map[ref_idx]->temporal_layer_id))
+                cm->ref_frame_map[ref_idx]->temporal_layer_id,
+                current_frame->mlayer_id))
           continue;
         if (!is_mlayer_scalable_and_dependent(
                 &cm->seq_params, current_frame->mlayer_id,

commit f45252a5f94fae55d09a6a7fbf7d333e1292b80a

Fix build due to rebase issue in previous commit
Add missing argument to `is_tlayer_scalable_and_dependent` in 2 places.

Issue was introduced in 8b5edc15

av2/decoder/decodeframe.c
@@ -7606,7 +7606,7 @@ void update_ref_frames_info(AV2Decoder *pbi, OBU_TYPE obu_type) {
       const int ref_mlayer_id = cm->ref_frame_map[idx]->mlayer_id;
       const int ref_temporal_id = cm->ref_frame_map[idx]->temporal_layer_id;
       if (is_tlayer_scalable_and_dependent(&cm->seq_params, cur_temporal_id,
-                                           ref_temporal_id) &&
+                                           ref_temporal_id, cur_mlayer_id) &&
           is_mlayer_scalable_and_dependent(&cm->seq_params, cur_mlayer_id,
                                            ref_mlayer_id)) {
         int map_pos = -1;
@@ -7856,7 +7856,8 @@ static int read_uncompressed_header(AV2Decoder *pbi, OBU_TYPE obu_type,
               const int ref_temporal_id =
                   cm->ref_frame_map[i]->temporal_layer_id;
               if (is_tlayer_scalable_and_dependent(
-                      &cm->seq_params, cur_temporal_id, ref_temporal_id) &&
+                      &cm->seq_params, cur_temporal_id, ref_temporal_id,
+                      cur_mlayer_id) &&
                   is_mlayer_scalable_and_dependent(
                       &cm->seq_params, cur_mlayer_id, ref_mlayer_id)) {
                 cm->ref_frame_map[i]->is_restricted = true;